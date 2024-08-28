ba424382610cf68030adc7dc6b3bb9379b89ac1c
Quocviet Le <quocvietlet@gmail.com>
2024-08-27 23:18:48 -0500

    train dataset under parameters

--- Random_Forest_Regression.py
+++ Random_Forest_Regression.py
@@ -12,3 +12,7 @@ from sklearn.ensemble import RandomForestRegressor
 dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+
+# Training the Random Forest Regression Model on the whole dataset
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(X, y)
